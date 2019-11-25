70b4d6d9385b38e3eeff886820564839baac7f93
viharika-allam <aruna.viharika@gmail.com>
2019-11-25 18:53:56 +0530

    atmmachine

---
+++ atmmachine/src/com/ustglobal/atmmachine/ICICI.java
@@ -0,0 +1,12 @@
+package com.ustglobal.atmmachine;
+
+public class ICICI implements ATM {
+	public void validateCard() {
+		System.out.println("icici validate card");
+	}
+	public void getInfo() {
+		System.out.println("icici get info");
+	}
+
+
+}
