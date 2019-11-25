87a177927338f9e7ed493be906fbf1eb09253a79
viharika-allam <aruna.viharika@gmail.com>
2019-11-25 19:05:23 +0530

    filehandling

---
+++ filehandling/src/com/ustglobal/filehandling/FileReadingClass.java
@@ -0,0 +1,22 @@
+package com.ustglobal.filehandling;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReadingClass {
+	public static void main(String[] args) {
+		String path = "C:\\Users\\Harika\\Desktop\\text.txt.txt";
+		FileReader reader = null;
+		try {
+		 reader = new FileReader(path);
+		 int i;
+		 while((i=reader.read())!=-1) {
+			 System.out.print((char)i);
+		 }
+	}catch(IOException e) {
+		e.printStackTrace();
+			}
+	}// end of main()
+
+
+}//end of FileReadingClass
---
+++ filehandling/src/com/ustglobal/filehandling/PropertiesFileReader.java
@@ -0,0 +1,26 @@
+package com.ustglobal.filehandling;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+public class PropertiesFileReader {
+	public static void main(String[] args) {
+		String path="text.properties";
+		FileReader reader = null;
+		try {
+			reader = new FileReader(path);
+			Properties properties = new Properties();
+			properties.load(reader);
+			String name = properties.getProperty("name");
+			String company =properties.getProperty("company");
+			String gf =properties.getProperty("gf");
+			
+			System.out.println("Name is "+name);
+			System.out.println("Company is "+company);
+			System.out.println("GF: "+gf);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
