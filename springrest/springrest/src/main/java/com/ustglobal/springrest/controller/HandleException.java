aee143ad86d88b70749508105482c7a186695c59
viharika-allam <aruna.viharika@gmail.com>
2019-12-04 15:06:01 +0530

    springrest

---
+++ springrest/springrest/src/main/java/com/ustglobal/springrest/controller/HandleException.java
@@ -0,0 +1,23 @@
+package com.ustglobal.springrest.controller;
+
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ustglobal.springrest.dto.EmployeeResponse;
+
+@RestControllerAdvice
+public class HandleException {
+	
+	@ExceptionHandler(Exception.class)
+	public  EmployeeResponse getException() {
+		
+		EmployeeResponse response = new EmployeeResponse();
+		response.setStatusCode(501);
+		response.setMessage("Error in code");
+		response.setDescription("Got an exception");
+		return response;
+	}
+
+}
